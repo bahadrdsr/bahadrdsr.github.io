1fda266b6e44f97421ca38320970c123e7f515f7
bahadır döşer <bahadirdoser@hotmail.com>
2018-10-15 23:42:57 +0300

    laptop setup

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 function Profile() {
+  const imgPath = process.env.PUBLIC_URL + "/assets/img/";
   return (
     <div className="profile">
       <div className="profile__image-box">
         <img
-          src="../assets/img/avatar-square.jpg"
+          src="/assets/img/avatar-square.jpg`}"
           alt=""
           className="profile__img"
         />
       </div>
-      <div className="description">
+      <div className="profile__description">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
         imperdiet quam non risus pharetra posuere. Donec a erat orci. Phasellus
         vehicula sapien sit amet elementum sagittis.
